72617c57c27227f8a35d8b3822fcd3727a7ac527
vineeth394 <munnavineeth@gmail.com>
2025-03-04 19:51:38 +0530

    Add files via upload

---
+++ Main.tf/import command.tf
@@ -0,0 +1,92 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+import {
+  to = aws_instance.web
+  id = "i-00cce0c8e20db034b"
+}
+
+# __generated__ by Terraform
+# Please review these resources and move them into your main configuration files.
+
+# __generated__ by Terraform
+resource "aws_instance" "web" {
+  ami                                  = "ami-00bb6a80f01f03502"
+  associate_public_ip_address          = true
+  availability_zone                    = "ap-south-1b"
+  disable_api_stop                     = false
+  disable_api_termination              = false
+  ebs_optimized                        = false
+  enable_primary_ipv6                  = null
+  get_password_data                    = false
+  hibernation                          = false
+  host_id                              = null
+  host_resource_group_arn              = null
+  iam_instance_profile                 = null
+  instance_initiated_shutdown_behavior = "stop"
+  instance_type                        = "t2.micro"
+  # ipv6_address_count                   = 0
+  # ipv6_addresses                       = []
+  key_name                             = "EC2 KEYPAIR"
+  monitoring                           = false
+  placement_group                      = null
+  placement_partition_number           = 0
+  private_ip                           = "172.31.15.190"
+  secondary_private_ips                = []
+  security_groups                      = ["launch-wizard-1"]
+  source_dest_check                    = true
+  subnet_id                            = "subnet-0ef8a66882d217563"
+  tags = {
+    Name = "instance_new"
+  }
+  tags_all = {
+    Name = "instance_new"
+  }
+  tenancy                     = "default"
+  user_data                   = null
+  user_data_base64            = null
+  user_data_replace_on_change = null
+  volume_tags                 = null
+  vpc_security_group_ids      = ["sg-0a1b219200f5f349c"]
+  capacity_reservation_specification {
+    capacity_reservation_preference = "open"
+  }
+  cpu_options {
+    amd_sev_snp      = null
+    core_count       = 1
+    threads_per_core = 1
+  }
+  credit_specification {
+    cpu_credits = "standard"
+  }
+  enclave_options {
+    enabled = false
+  }
+  maintenance_options {
+    auto_recovery = "default"
+  }
+  metadata_options {
+    http_endpoint               = "enabled"
+    http_protocol_ipv6          = "disabled"
+    http_put_response_hop_limit = 2
+    http_tokens                 = "required"
+    instance_metadata_tags      = "disabled"
+  }
+  private_dns_name_options {
+    enable_resource_name_dns_a_record    = true
+    enable_resource_name_dns_aaaa_record = false
+    hostname_type                        = "ip-name"
+  }
+  root_block_device {
+    delete_on_termination = true
+    encrypted             = false
+    iops                  = 3000
+    kms_key_id            = null
+    tags                  = {}
+    tags_all              = {}
+    throughput            = 125
+    volume_size           = 8
+    volume_type           = "gp3"
+  }
+}
